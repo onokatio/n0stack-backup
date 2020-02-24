40d42eabfa83da2dd22ab602d0bbf0c218f93980
onokatio <onokatio@gmail.com>
2020-02-24 19:07:56 +0900

    fix jq bug

--- main.sh
+++ main.sh
@@ -1,11 +1,17 @@
-list=$(n0cli get bs -o json \
-    | jq -cr '.block_storages[]' \
-    | jq -cr 'select(.state == "PROTECTED")' \
-    | jq -cr '[.name, .annotations."n0core/provisioning/block_storage/request_node_name"]' \
-    | sed -e 's/ictsc-sl2500-0/172.16.1.1/')
+#!/usr/bin/zsh
 
-for bs in $list;do
+export N0CLI_API_URL=grpc://172.16.1.11:20180
+export N0CLI_API_ENDPOINT=172.16.1.11:20180
+
+n0cli get bs -o json \
+    | jq '.block_storages[]' \
+    | jq 'select(.state == "PROTECTED")' \
+    | jq -cr '[.name, .annotations."n0core/provisioning/block_storage/request_node_name"]' \
+    | sed -e 's/ictsc-sl2500-0/172.16.1.1/' > json
+
+cat json | while read line;do
 	set -v
-	echo rsync -av -e "ssh -i /fio/sl2500-root.key" root@$(echo $bs | jq -r .[1]):/var/lib/n0core/block_storage/$(echo $bs | jq -r .[0]) /fio/block_storage/
+	rsync -av -e "ssh -i /fio/sl2500-root.key" root@$(echo "$line" | jq -r '.[1]'):/var/lib/n0core/block_storage/$(echo "$line" | jq -r '.[0]') /fio/block_storage/
 	set +v
 done
+
